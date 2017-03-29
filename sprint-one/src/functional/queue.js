00cd60b64dcd09d56c7c53c3fbc31eba47ad9c2c
Hack Reactor Student <student@hackreactor.com>
2017-03-29 15:15:11 -0700

    Complete queue function
    All tests pass for queue function.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,33 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
+  var dequeueKey = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.dequeue = function() {
+    if (count - dequeueKey > 0) {
+      // count--;
+      dequeueKey++;
+    }
+    // debugger;
+    return storage[dequeueKey - 1];
   };
 
   someInstance.size = function() {
+    return count - dequeueKey;
   };
 
   return someInstance;
 };
+
+// {0:'a', 0:'b'}
+// dequeue --> return 'a'
+// {1: b, 2: c}
+
