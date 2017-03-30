8602c9b3b2dca6d6c8bcead6a2d4946c8f339da5
Charles Kim <cjkim0119@gmail.com>
2017-03-29 19:52:05 -0700

    Complete Data Structures part 1. Passed all tests.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,22 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
+  this.dequeueKey = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.count] = value; 
+  this.count++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.count - this.dequeueKey > 0) {
+    this.dequeueKey++;
+  }
+  return this.storage[this.dequeueKey - 1];
+};
+
+Queue.prototype.size = function() {
+  return this.count - this.dequeueKey;
+};
 
