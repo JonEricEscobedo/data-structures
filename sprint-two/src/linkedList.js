094dfe6e6c84c9bb90bf0209ec4f85714818b58a
Hack Reactor Student <student@hackreactor.com>
2017-03-30 16:05:46 -0700

    Complete implementing linkedList function. Passed all tests.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.tail !== null) { 
+      list.head = list.tail;
+      list.tail.next = newNode;
+    }
+
+    list.tail = newNode;
+
+    if (list.head === null) {
+      list.head = list.tail;
+    }
+
   };
 
   list.removeHead = function() {
+    var oldHeadData = list.head.value;
+    list.head = list.head.next;
+    return oldHeadData;
   };
 
   list.contains = function(target) {
+    var truthy = false;
+
+    var searchList = function(node) {
+      if (node.value === target) {
+        truthy = true;
+      } else if (node.next !== null) {
+        searchList(node.next);
+      }
+    };
+    searchList(list.head);
+    return truthy;
   };
 
   return list;
