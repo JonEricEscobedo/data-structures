946aa70468cbed124cbd11528e48321bb0514258
Hack Reactor Student <student@hackreactor.com>
2017-03-31 11:15:29 -0700

    Complete implementing binarySearchTree function. Passed all tests.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -9,20 +9,72 @@ var BinarySearchTree = function(value) {
 };
 
 BinarySearchTree.prototype.insert = function (val) {
-	if (val > this.value) {
-		this.right = BinarySearchTree(val);
-	} else if (val < this.value) {
-		this.left = BinarySearchTree(val);
-	}
+	var newNode = BinarySearchTree(val);
+
+	var searchNodeValues = function(parentNode) {
+		if (newNode.value < parentNode.value) {
+			if (parentNode.left === null) {
+				parentNode.left = newNode;
+			} else {
+				searchNodeValues(parentNode.left);
+			}
+		} else if (newNode.value > parentNode.value) {
+			if (parentNode.right === null) {
+				parentNode.right = newNode;
+			} else {
+				searchNodeValues(parentNode.right);
+			}
+		}
+	};
+	searchNodeValues(this);
+	
 };
 
-BinarySearchTree.prototype.contains = function () {
+BinarySearchTree.prototype.contains = function (target) {
+	var truthy = false;
 
+	var searchNodes = function(parentNode) {
+		if (target < parentNode.value && parentNode.left) {
+			if (parentNode.left.value === target) {
+				truthy = true;
+			} else if (parentNode.left) {
+				searchNodes(parentNode.left);
+			}
+		} else if (target > parentNode.value && parentNode.right) {
+			if (parentNode.right.value === target) {
+				truthy = true;
+			} else if (parentNode.right) {
+				searchNodes(parentNode.right);
+			}
+		}
+	};
+	searchNodes(this);
+
+	return truthy;
 };
 
-BinarySearchTree.prototype.depthFirstLog = function () {
+BinarySearchTree.prototype.depthFirstLog = function (callback) {
+
+	var depthSearch = function(node) {
+		callback(node.value);
+
+		if (node.left) {
+			depthSearch(node.left);
+		}
+		if (node.right) {
+			depthSearch(node.right);
+		}
+		
+	};
+
+	depthSearch(this);
 
 };
 /*
  * Complexity: What is the time complexity of the above functions?
+ *
+ * Answer:
+ * insert method has logarithmic time complexity.
+ * contains method has logarithmic time complexity.
+ * depthFirstLog has linear time complexity.
  */
