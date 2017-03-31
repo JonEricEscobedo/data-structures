5377f9ccba5fb3d8c78232a37fc90eb9a0fe6e78
Hack Reactor Student <student@hackreactor.com>
2017-03-30 17:38:57 -0700

    Completed implementing tree function. Passed all tests.
    Need to complete time complexity analysis.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,9 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,9 +12,37 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+	var newChild = Tree(value);
+	this.children.push(newChild);
 };
 
 treeMethods.contains = function(target) {
+	var truthy = false;
+
+	var searchChildren = function(child) {
+		for (var i = 0; i < child.children.length; i++) {
+			if (child.children[i].value === target) {
+				truthy = true;
+			} else {
+				searchChildren(child.children[i]);
+			}
+		}
+	}
+
+
+	// 	// Base case
+	// 	if (child.value === target) {
+	// 		truthy = true;
+	// 	} else {
+	// 		// Recursive case
+	// 		for (var idx = 0; idx < this.children.length; idx++) {
+	// 			searchChildren(this.children[idx]);
+	// 		}
+	// 	}
+		
+	// }
+	searchChildren(this);
+	return truthy;
 };
 
 
