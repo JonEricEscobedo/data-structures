72db0b2f991554ae8bbcb4ea3693b1e39d044143
Hack Reactor Student <student@hackreactor.com>
2017-04-01 11:25:26 -0700

    Attempt advanced requirements for hashTable function implementation. WIP.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,7 @@
 var HashTable = function() {
   this._limit = 8;
+  // this._tupleCount = 0;
+  // this._oldStorage = null;
   this._storage = LimitedArray(this._limit);
 };
 
@@ -16,11 +18,31 @@ HashTable.prototype.insert = function(k, v) {
     }
   }
 
+  // Double size when needed
+  // this._tupleCount++;
+
+  // if (this._tupleCount > .75 * this._limit) {
+  //   this._limit *= 2;
+  //   this._oldStorage = this._storage;
+  //   this._storage = LimitedArray(this._limit);
+  //   this._oldStorage.each(function(val, i, collection) {
+  //     if (val) {
+  //       for (var i = 0; i < val.length; i++) {
+  //         this.insert()
+  //       }
+  //     }
+  //   })
+  //   // create a old_storage var = current _storage
+  //   // create a new this._storage = LimitedArray(this._limit);
+  //   // this.old_storage.each(function(val, i, collection) {this.insert(val)})
+
+
   if (replaced === false) {
     bucketArr.push(args);
   }
 
   this._storage.set(index, bucketArr);
+
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -44,6 +66,7 @@ HashTable.prototype.remove = function(k) {
   for (var i = 0; i < bucketArrLength; i++) {
     if (bucketArr[i][0] === k) {
       bucketArr.splice(i, 1);
+      this._tupleCount--;
     }
   }
 };
