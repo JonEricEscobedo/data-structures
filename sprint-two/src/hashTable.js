807b64d1bceecfc80593928588793c0913d7af03
Hack Reactor Student <student@hackreactor.com>
2017-04-01 15:09:51 -0700

    Implement hashtable resize function. Passed all tests including the advanced portion.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,7 +1,6 @@
 var HashTable = function() {
   this._limit = 8;
-  // this._tupleCount = 0;
-  // this._oldStorage = null;
+  this._tupleCount = 0;
   this._storage = LimitedArray(this._limit);
 };
 
@@ -13,29 +12,10 @@ HashTable.prototype.insert = function(k, v) {
   var replaced = false;
   for (var i = 0; i < bucketArr.length; i++) {
     if (bucketArr[i][0] === k) {
-      bucketArr[i] = args;
+      bucketArr[i] = [k, v];
       replaced = true;
     }
   }
-
-  // Double size when needed
-  // this._tupleCount++;
-
-  // if (this._tupleCount > .75 * this._limit) {
-  //   this._limit *= 2;
-  //   this._oldStorage = this._storage;
-  //   this._storage = LimitedArray(this._limit);
-  //   this._oldStorage.each(function(val, i, collection) {
-  //     if (val) {
-  //       for (var i = 0; i < val.length; i++) {
-  //         this.insert()
-  //       }
-  //     }
-  //   })
-  //   // create a old_storage var = current _storage
-  //   // create a new this._storage = LimitedArray(this._limit);
-  //   // this.old_storage.each(function(val, i, collection) {this.insert(val)})
-
 
   if (replaced === false) {
     bucketArr.push(args);
@@ -43,11 +23,17 @@ HashTable.prototype.insert = function(k, v) {
 
   this._storage.set(index, bucketArr);
 
+  this._tupleCount++;
+
+  if (this._tupleCount > .75 * this._limit) {
+    this.resize(this._limit * 2);
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucketArr = this._storage.get(index);
+  var bucketArr = this._storage.get(index) || [];
   var bucketArrLength = bucketArr.length;
 
   for (var i = 0; i < bucketArrLength; i++) {
@@ -69,9 +55,28 @@ HashTable.prototype.remove = function(k) {
       this._tupleCount--;
     }
   }
+
+  if (this._tupleCount < .25 * this._limit) {
+    this.resize(this._limit / 2);
+  }  
 };
 
+HashTable.prototype.resize = function(newLimit) {
+  var oldStorage = this._storage;
 
+  this._storage = LimitedArray(newLimit);
+  this._tupleCount = 0;
+  this._limit = newLimit;
+
+  oldStorage.each(function(bucket) {
+    if (bucket) {
+      for (var i = 0; i < bucket.length; i++) {
+        this.insert(bucket[i][0], bucket[i][1]);
+      }
+    }
+  }.bind(this));
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
@@ -80,6 +85,7 @@ HashTable.prototype.remove = function(k) {
  * insert has linear time complexity
  * retrieve has linear time complexity
  * remove has linear time complexity
+ * resize has linear time complexity
  */
 
 
